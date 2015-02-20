d9484b3bf001ce0b11970ea8c26c2d9f58be7925
cyrilpanicker <cyrilpanicker@gmail.com>
2015-02-20 10:19:14 +0530

    code changes

--- server.js
+++ server.js
@@ -1,22 +1,12 @@
 var express = require('express');
-var request = require('request')
 
 var app=express();
 
-app.post('/getAllJiras',function (req,res) {
-	var data;
-	var pipe = req.pipe(request.post('https://obujira.searshc.com/jira/rest/api/2/search'));
-	pipe.on('data',function (chunk) {
-		data += chunk;
-	});
-	pipe.on('end',function () {
-		res.send(data);
-	});
-});
+app.get('/jira/*',function (req,res) {
+	request('http://public-api.wordpress.com/'+req.originalUrl.slice(6)).pipe(res);
+})
 
 app.use(express.static('public'));
-
-
 
 app.listen(8000,function () {
 	console.log('server listening at port 8000');
